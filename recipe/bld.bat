99549966f5e0ab41758ce9d75748de91004c6ab9
toreaadland <tore.aadland@cegal.com>
2020-03-18 15:17:02 +0100

    Remove all experimental changes

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,5 @@
 setlocal EnableDelayedExpansion
 
-:: Remove -GL from CXXFLAGS as this causes a fatal error
-:: See https://github.com/conda/conda-build/issues/2850
-set "CXXFLAGS=%CXXFLAGS:-GL=%"
-set
 set VERBOSE=1
 
 :: Make a build folder and change to it.
@@ -12,15 +8,14 @@ cd build
 
 :: Configure using the CMakeFiles
 cmake -G "NMake Makefiles" ^
-    -DCMAKE_BUILD_TYPE=Release ^
     -DBUILD_ZFPY=ON ^
     -DZFP_WITH_OPENMP=OFF ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     ..
 if errorlevel 1 exit 1
 
-cmake --build . --config Release
+nmake
 if errorlevel 1 exit 1
 
-cmake --build . --target install
+nmake install
 if errorlevel 1 exit 1
