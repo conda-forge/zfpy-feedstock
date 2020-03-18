b38958eff6f269c3380a14639416627eee816839
toreaadland <tore.aadland@cegal.com>
2020-03-18 13:21:15 +0100

    Add windows build configuration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,20 @@ mkdir build
 cd build
 
 :: Configure using the CMakeFiles
-cmake -GNinja -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ..
+cmake -G "NMake Makefiles" ^
+    -DBUILD_ZFPY=ON ^
+    -DZFP_WITH_OPENMP=OFF ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    ..
 if errorlevel 1 exit 1
 
-:: Build and Install
-cmake --build . --target install --config Release
+:: Build
+nmake
+if errorlevel 1 exit 1
+
+:: Install
+nmake install
 if errorlevel 1 exit 1
 
 :: Run tests
-ctest
 bin/testzfp
