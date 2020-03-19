06aa752baf36ad27490dd6d9c82f7bef7fd91e66
toreaadland <tore.aadland@cegal.com>
2020-03-19 16:10:44 +0100

    And no shared lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,14 +19,14 @@ echo "Nmake thinks that Cmake is located at: "
 echo %BUILD_PREFIX%\Library\bin\cmake.exe 
 
 :: Configure using the CMakeFiles
-REM cmake --build . --target INSTALL --config Release
+  REM -DZFP_WITH_OPENMP=OFF ^
+    REM -DBUILD_SHARED_LIBS=ON ^
 cmake -G "Ninja" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
   -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
   -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
   -DBUILD_TESTING=OFF ^
-  -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   ..
 if errorlevel 1 exit 1
