1e803e7e8418f5175efb71998113939b199a8087
toreaadland <tore.aadland@cegal.com>
2020-03-19 16:09:05 +0100

    ffaffing about

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,20 +19,19 @@ echo "Nmake thinks that Cmake is located at: "
 echo %BUILD_PREFIX%\Library\bin\cmake.exe 
 
 :: Configure using the CMakeFiles
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  ::-DBUILD_ZFPY=ON ^
   -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
   -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
   -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
-  -DZFP_WITH_OPENMP=ON ^
+  -DZFP_WITH_OPENMP=OFF ^
   -DBUILD_TESTING=OFF ^
   -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
   ..
 if errorlevel 1 exit 1
 
-nmake install /D /P
+cmake --build . --target INSTALL --config Release
 if errorlevel 1 exit 1
 
 popd
