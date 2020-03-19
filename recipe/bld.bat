710d678a9ff5a6fa5d54d12c123075d50e8f5bec
toreaadland <tore.aadland@cegal.com>
2020-03-19 16:09:40 +0100

    Try witout building python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -19,12 +19,12 @@ echo "Nmake thinks that Cmake is located at: "
 echo %BUILD_PREFIX%\Library\bin\cmake.exe 
 
 :: Configure using the CMakeFiles
+REM cmake --build . --target INSTALL --config Release
 cmake -G "Ninja" ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
   -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
   -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
-  -DZFP_WITH_OPENMP=OFF ^
   -DBUILD_TESTING=OFF ^
   -DBUILD_SHARED_LIBS=ON ^
   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
