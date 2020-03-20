c20148d383f860d78d650be9e00f1e5381b19fdb
toreaadland <tore.aadland@cegal.com>
2020-03-20 11:39:04 +0100

    Try building a hello world

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,31 +1,40 @@
 setlocal EnableDelayedExpansion
 
-:: Remove -GL from CXXFLAGS as this causes a fatal error
-:: See https://github.com/conda/conda-build/issues/2850
-:: set "CXXFLAGS=%CXXFLAGS:-GL=%"
-set "CXXFLAGS=%CXXFLAGS:-GL=%"
-set VERBOSE=1
+git clone https://github.com/jameskbride/cmake-hello-world.git
 
-:: Make a build folder and change to it.
+cd cmake-hello-world
+
 mkdir build
 cd build
+cmake -G "NMake Makefiles" ..
+nmake
 
-set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+REM :: Remove -GL from CXXFLAGS as this causes a fatal error
+REM :: See https://github.com/conda/conda-build/issues/2850
+REM :: set "CXXFLAGS=%CXXFLAGS:-GL=%"
+REM set "CXXFLAGS=%CXXFLAGS:-GL=%"
+REM set VERBOSE=1
 
-:: Configure using the CMakeFiles
-cmake -G "Ninja" ^
-  -DCMAKE_BUILD_TYPE=Release ^
-  -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
-  -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
-  -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
-  -DBUILD_TESTING=OFF ^
-  -DBUILD_ZFPY=ON ^
-  -DZFP_WITH_OPENMP=OFF ^
-  -DBUILD_SHARED_LIBS=ON ^
-  -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
-  -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-  ..
-if errorlevel 1 exit 1
+REM :: Make a build folder and change to it.
+REM mkdir build
+REM cd build
 
-cmake --build . --target install --config Release
-if errorlevel 1 exit 1
+REM set PYTHON_LIBRARY=%PREFIX%\libs\python%PY_VER:~0,1%%PY_VER:~2,1%.lib
+
+REM :: Configure using the CMakeFiles
+REM cmake -G "Ninja" ^
+REM   -DCMAKE_BUILD_TYPE=Release ^
+REM   -DPYTHON_EXECUTABLE:FILEPATH="%PYTHON%" ^
+REM   -DPYTHON_INCLUDE_DIR:PATH="%PREFIX%\include" ^
+REM   -DPYTHON_LIBRARY:FILEPATH="%PYTHON_LIBRARY%" ^
+REM   -DBUILD_TESTING=OFF ^
+REM   -DBUILD_ZFPY=ON ^
+REM   -DZFP_WITH_OPENMP=OFF ^
+REM   -DBUILD_SHARED_LIBS=ON ^
+REM   -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON ^
+REM   -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+REM   ..
+REM if errorlevel 1 exit 1
+
+REM cmake --build . --target install --config Release
+REM if errorlevel 1 exit 1
