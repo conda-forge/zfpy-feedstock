620e95672e1058bc81e07cad23e444eafa0aec0f
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-07 05:19:37 -0700

    Add compilation options in the linux and osx cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 set -e
+SHORT_OS_STR=$(uname -s)
+if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
+    OPENMP="-DZFP_WITH_OPENMP=1"
+fi
+if [ "${SHORT_OS_STR}" == "Darwin" ]; then
+    OPENMP=""
+fi
 
 mkdir build
 cd build
-cmake -DBUILD_ZFPY=ON -DZFP_WITH_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_INSTALL_LIBDIR=lib ..
+cmake                              \
+  -DBUILD_CFP=ON                   \
+  -DBUILD_ZFPY=ON                  \
+  -DBUILD_UTILITIES=ON             \
+  ${OPENMP}                        \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DCMAKE_PREFIX_PATH=${PREFIX}    \
+  -DCMAKE_INSTALL_LIBDIR=lib       \
+  ..
+
 make -j${CPU_COUNT}
 make test
 make install
+# Binary not install correctly by cmake
+install bin/zfp ${PREFIX}/bin/.
 
 ./bin/testzfp
+
