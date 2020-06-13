899b614ace43bc77ea6deef0473f2b4938ac8566
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-13 13:19:41 -0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -18,6 +18,7 @@ cmake                              \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX}    \
   -DCMAKE_INSTALL_LIBDIR=lib       \
+  -DPython_ROOT_DIR=${PREFIX}      \
   ..
 
 make -j${CPU_COUNT}
