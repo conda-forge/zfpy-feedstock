de15a26d9423419f762ff24863b3d8b1391fa3e4
Mark Harfouche <mark.harfouche@gmail.com>
2020-06-13 20:49:08 -0400

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -26,16 +26,12 @@ if errorlevel 1 exit 1
 ninja install
 if errorlevel 1 exit 1
 
-:: Custom installation step
-REM copy "python\lib\zfpy.pyd" "%PREFIX%\Lib\site-packages"
-REM if errorlevel 1 exit 1
-REM copy "bin\zfp.dll" "%PREFIX%\Lib\site-packages"
-REM if errorlevel 1 exit 1
-
 REM command line utility not installed for some reason
 copy "bin\zfp.exe" "%LIBRARY_BIN%\."
 if errorlevel 1 exit 1
 
 :: Run tests
 ctest
+if errorlevel 1 exit 1
 bin\testzfp.exe
+if errorlevel 1 exit 1
