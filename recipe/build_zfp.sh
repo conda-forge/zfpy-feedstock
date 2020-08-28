eab8bb9024710346dd0a25ba6aa1e79edfe45c8f
Mark Harfouche <mark.harfouche@gmail.com>
2020-08-28 14:12:01 -0400

    Update build_zfp.sh

--- recipe/build_zfp.sh
+++ recipe/build_zfp.sh
@@ -16,7 +16,7 @@ cmake                              \
   ${OPENMP}                        \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX}    \
-  -DCMAKE_INSTALL_LIBDIR=lib.      \
+  -DCMAKE_INSTALL_LIBDIR=lib \
   ..
 
 make -j${CPU_COUNT}
