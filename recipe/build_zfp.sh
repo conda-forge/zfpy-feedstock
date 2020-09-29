99449292570ea113b4bcf1956f8b0b533d70348a
Axel Huebl <axel.huebl@plasma.ninja>
2020-09-29 01:14:26 -0700

    Minor Updates
    Adds minor updates:
    - OpenMP support on macOS
    - check for installed CMake files

--- recipe/build_zfp.sh
+++ recipe/build_zfp.sh
@@ -1,19 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-SHORT_OS_STR=$(uname -s)
-if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
-    OPENMP="-DZFP_WITH_OPENMP=1"
-fi
-if [ "${SHORT_OS_STR}" == "Darwin" ]; then
-    OPENMP="-DZFP_WITH_OPENMP=0"
-fi
 
 mkdir build
 cd build
 cmake                              \
   -DBUILD_CFP=ON                   \
   -DBUILD_UTILITIES=ON             \
-  ${OPENMP}                        \
+  -DZFP_WITH_OPENMP=ON             \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DCMAKE_PREFIX_PATH=${PREFIX}    \
   -DCMAKE_INSTALL_LIBDIR=lib       \
--- recipe/build_zfpy.sh
+++ recipe/build_zfpy.sh
@@ -1,11 +1,5 @@
+#!/usr/bin/env bash
 set -e
-SHORT_OS_STR=$(uname -s)
-if [ "${SHORT_OS_STR:0:5}" == "Linux" ]; then
-    OPENMP="-DZFP_WITH_OPENMP=1"
-fi
-if [ "${SHORT_OS_STR}" == "Darwin" ]; then
-    OPENMP="-DZFP_WITH_OPENMP=0"
-fi
 
 #  hmaarrfk: 2020/06/20
 #  Basically, this build is going to reinstall the C libraries
@@ -18,9 +12,9 @@ mkdir build
 cd build
 cmake                              \
   -DBUILD_CFP=ON                   \
-  -DBUILD_UTILITIES=ON            \
+  -DBUILD_UTILITIES=ON             \
   -DBUILD_ZFPY=ON                  \
-  ${OPENMP}                        \
+  -DZFP_WITH_OPENMP=ON             \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DPython_ROOT_DIR=${PREFIX}      \
   -DPython_FIND_VIRTUALENV=ONLY    \
