180cca9da45f61f7ed3dfad13a9e68e3d00006f8
Mark Harfouche <mark.harfouche@gmail.com>
2020-10-11 16:33:52 -0400

    Hack flags for pypy

--- recipe/build_zfpy.sh
+++ recipe/build_zfpy.sh
@@ -10,17 +10,23 @@ set -e
 rm -rf build
 mkdir build
 cd build
+
+EXTRA_FLAGS=
+if [ `${PYTHON} --version | grep PyPy` ]; then
+    EXTRA_FLAGS="-DPYTHON_LIBRARY=${PREFIX}/pypy/lib/libpypy3-c.so"
+fi
 cmake                              \
   -DBUILD_CFP=ON                   \
   -DBUILD_UTILITIES=ON             \
   -DBUILD_ZFPY=ON                  \
   -DZFP_WITH_OPENMP=ON             \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DPython_ROOT_DIR=${PREFIX}      \
+  -DPython_FIND_VIRTUALENV=ONLY    \
   -DCMAKE_INSTALL_LIBDIR=lib       \
+  ${EXTRA_FLAGS}                   \
   ..
 
-  # -DPython_ROOT_DIR=${PREFIX}
-  # -DPython_FIND_VIRTUALENV=ONLY
 make
 make install
 
