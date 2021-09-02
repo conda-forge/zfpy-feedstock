7cd84289802359482464af56b82ee364bb75e3b1
Mark Harfouche <mark.harfouche@gmail.com>
2021-09-01 23:15:00 -0400

    literal?

--- recipe/bld_zfpy.bat
+++ recipe/bld_zfpy.bat
@@ -2,8 +2,8 @@ setlocal EnableDelayedExpansion
 
 del /F/Q/S build
 
-type python\zfpy.pyx | findstr /v "from cpython" >  python\zfpy.pyx_temp
-type python\zfpy.pyx_temp | findstr /v "import array" > python\zfpy.pyx
+type python\zfpy.pyx | findstr /v /l "from cpython" >  python\zfpy.pyx_temp
+type python\zfpy.pyx_temp | findstr /v /l "import array" > python\zfpy.pyx
 del python\zfpy.pyx_temp
 
 :: Make a build folder and change to it.
